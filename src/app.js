2c1474ac5fd337e46b2aa0cb7aa58d27fa943139
Mic Neale <michael.neale@gmail.com>
2023-02-02 19:43:40 +1100

    adding a health check

--- src/app.js
+++ src/app.js
@@ -6,6 +6,11 @@ export const app = express();
 
 app.use(cors());
 
+app.get('/health', (req, res) => {
+  // return 200 ok
+  return res.json({ ok: true });
+});
+
 app.post('/', express.json({ type: '*/*' }), async (req, res) => {
   console.log(req.body);
   const result = await rpcRouter.handle(req.body, { transport: 'http' });
