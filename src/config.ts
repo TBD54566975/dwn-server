dccbf4a2699219f0edb175b5f24caf8cbe7b8c55
Moe Jangda <moe@tbd.email>
2023-05-01 08:25:24 -0500

    fix  config

--- src/config.ts
+++ src/config.ts
@@ -8,7 +8,7 @@ export const config = {
   // port that server listens on
   port                   : parseInt(process.env.DS_PORT || '3000'),
   // whether to enable 'ws:'
-  webSocketServerEnabled : { 'on': true, 'off': false}[process.env.DS_WEBSOCKET_SERVER] || true
+  webSocketServerEnabled : { 'on': true, 'off': false }[process.env.DS_WEBSOCKET_SERVER] ?? true
   // TODO: add config option to change data path. will need to change dwn.ts to ensure that the option works
   //       inside and outside a docker container.
 };
