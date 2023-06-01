d4a953d26125569ee3fc2be7992c6c03e21f7fe9
Moe Jangda <moe@tbd.email>
2023-06-01 14:06:26 -0500

    Tombstones (#31)

--- src/json-rpc-handlers/dwn/process-message.ts
+++ src/json-rpc-handlers/dwn/process-message.ts
@@ -2,17 +2,29 @@ import type { Readable as IsomorphicReadable } from 'readable-stream';
 import type { JsonRpcHandler, HandlerResponse } from '../../lib/json-rpc-router.js';
 
 import { v4 as uuidv4 } from 'uuid';
+import { DwnInterfaceName, DwnMethodName } from '@tbd54566975/dwn-sdk-js';
 
 import { JsonRpcErrorCodes, createJsonRpcErrorResponse, createJsonRpcSuccessResponse } from '../../lib/json-rpc.js';
 
 export const handleDwnProcessMessage: JsonRpcHandler = async (dwnRequest, context) => {
   let { dwn, dataStream } = context;
   const { target, message } = dwnRequest.params;
-
   const requestId = dwnRequest.id ?? uuidv4();
 
   try {
-    const reply = await dwn.processMessage(target, message, dataStream as IsomorphicReadable);
+    let reply;
+    const messageType = message?.descriptor?.interface + message?.descriptor?.method;
+
+    // When a record is deleted via `RecordsDelete`, the initial RecordsWrite is kept as a tombstone _in addition_
+    // to the RecordsDelete message. the data associated to that initial RecordsWrite is deleted. If a record was written
+    // _and_ deleted before it ever got to dwn-server, we end up in a situation where we still need to process the tombstone
+    // so that we can process the RecordsDelete.
+    if (messageType === DwnInterfaceName.Records + DwnMethodName.Write && !dataStream) {
+      reply = await dwn.synchronizePrunedInitialRecordsWrite(target, message);
+    } else {
+      reply = await dwn.processMessage(target, message, dataStream as IsomorphicReadable);
+    }
+
 
     // RecordsRead messages return record data as a stream to for accommodate large amounts of data
     let recordDataStream;
