1ed8a578459bb8321d1009dc8772a823722a539c
Adam Leos <aleos@aleos-mac.local>
2023-08-28 11:08:20 -0700

    readme update and index exports

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,6 @@
-export { DwnServer } from './dwn-server.js';
+export { Config } from './config.js';
+export { DwnServer, DwnServerOptions } from './dwn-server.js';
+export { HttpApi } from './http-api.js';
+export { jsonRpcApi } from './json-rpc-api.js';
+export { EStoreType, BackendTypes, StoreType } from './storage.js';
+export { WsApi } from './ws-api.js';
