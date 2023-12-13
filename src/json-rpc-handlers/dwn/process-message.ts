a55711f5bf71a82945c9bb8e9c184f3e2191555e
Henry Tsai <henrytsai@squareup.com>
2023-12-13 13:21:27 -0800

    Updated `dwn-sdk-js` and `dwn-sql-store` dependencies + v0.1.8 (#95)
    Updated dependencies + v0.1.8

--- src/json-rpc-handlers/dwn/process-message.ts
+++ src/json-rpc-handlers/dwn/process-message.ts
@@ -1,5 +1,4 @@
 import type { RecordsReadReply } from '@tbd54566975/dwn-sdk-js';
-import { DwnInterfaceName, DwnMethodName } from '@tbd54566975/dwn-sdk-js';
 
 import type { Readable as IsomorphicReadable } from 'readable-stream';
 import { v4 as uuidv4 } from 'uuid';
@@ -23,26 +22,11 @@ export const handleDwnProcessMessage: JsonRpcHandler = async (
   const requestId = dwnRequest.id ?? uuidv4();
 
   try {
-    let reply;
-    const messageType =
-      message?.descriptor?.interface + message?.descriptor?.method;
-
-    // When a record is deleted via `RecordsDelete`, the initial RecordsWrite is kept as a tombstone _in addition_
-    // to the RecordsDelete message. the data associated to that initial RecordsWrite is deleted. If a record was written
-    // _and_ deleted before it ever got to dwn-server, we end up in a situation where we still need to process the tombstone
-    // so that we can process the RecordsDelete.
-    if (
-      messageType === DwnInterfaceName.Records + DwnMethodName.Write &&
-      !dataStream
-    ) {
-      reply = await dwn.synchronizePrunedInitialRecordsWrite(target, message);
-    } else {
-      reply = (await dwn.processMessage(
-        target,
-        message,
-        dataStream as IsomorphicReadable,
-      )) as RecordsReadReply;
-    }
+    const reply = (await dwn.processMessage(
+      target,
+      message,
+      dataStream as IsomorphicReadable,
+    )) as RecordsReadReply;
 
     // RecordsRead messages return record data as a stream to for accommodate large amounts of data
     let recordDataStream;
