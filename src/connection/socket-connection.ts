7bb207a1c9ba8670901ffd10943dc57a29815613
Liran Cohen <c.liran.c@gmail.com>
2024-02-14 17:34:14 -0500

    rename onError to callback

--- src/connection/socket-connection.ts
+++ src/connection/socket-connection.ts
@@ -165,16 +165,16 @@ export class SocketConnection {
   }
 
   /**
-   * Sends a JSON encoded Buffer through the Websocket. Accepts an error handler, if none is provided the error is logged.
+   * Sends a JSON encoded Buffer through the Websocket. Accepts a callback, if none is provided an error logger is used.
    */
-  private send(response: JsonRpcResponse | JsonRpcErrorResponse, onError?: (error?: Error) => void): void {
-    if (!onError) {
-      onError = (error):void => {
+  private send(response: JsonRpcResponse | JsonRpcErrorResponse, cb?: (error?: Error) => void): void {
+    if (!cb) {
+      cb = (error):void => {
         if(error) { log.error('socket send error', error, response); }
       }
     }
 
-    this.socket.send(Buffer.from(JSON.stringify(response)), onError);
+    this.socket.send(Buffer.from(JSON.stringify(response)), cb);
   }
 
   /**
